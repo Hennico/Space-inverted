6536b748105f3e7253b96468723cc170f8e43de1
Hennico <nicolas_zanardo@hotmail.com>
2018-09-22 15:41:14 -0300

    base disparador

---
+++ Assets/In Game/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(0, 1));
+
+        if (hit.collider != null) {
+            Debug.Log("pew pew pew");
+        }
+	}
+}
